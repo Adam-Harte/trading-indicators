7d7b78b44b1cba9f57f204accc9e61cccac73150
Adam Harte <adamh@CA-MBP-458.local>
2022-06-19 22:34:04 +0100

    build(jest and ts): jest and ts configs updated

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  transform: {},
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
+
+export default config;
