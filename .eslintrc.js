7d2a3cadd1fa5d0809fcfb0de1771cadf55f0441
Adam Harte <adamh@CA-MBP-458.local>
2022-06-20 22:17:50 +0100

    build(eslint): eslint config updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   extends: [
     'airbnb-typescript/base',
+    'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
     'eslint:recommended',
   ],
