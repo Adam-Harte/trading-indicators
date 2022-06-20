e74028070e190fdf31794a6eb7349f00c25aea48
Adam Harte <adamh@CA-MBP-458.local>
2022-06-20 11:58:24 +0100

    build(eslint): eslint config updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,31 @@
 module.exports = {
   root: true,
-  extends: 'airbnb-typescript/base',
-  plugins: ['import', 'prettier'],
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'prettier', 'jest'],
   parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
     project: './tsconfig.eslint.json',
   },
-}
+  settings: {
+    'import/extensions': ['.js', '.ts'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+};
