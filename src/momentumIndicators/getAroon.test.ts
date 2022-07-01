0b40d0ac0edc6330a4f57acd2771e8fc02412761
Adam Harte <adamh@CA-MBP-458.local>
2022-07-01 16:43:18 +0100

    test(getaroon): test coverage added for getAroon function

---
+++ src/momentumIndicators/getAroon.test.ts
@@ -0,0 +1,27 @@
+import { getAroon } from './getAroon';
+
+const highs = [
+  11.12, 21.57, 13.31, 19.94, 18.2, 10.81, 12.21, 14.95, 17.42, 13.46, 11.12,
+  22.04, 13.31, 19.94, 18.2, 10.73, 12.21, 14.95, 16.39, 13.46, 11.12, 21.57,
+  13.31, 19.94, 16.2, 15.26, 12.21, 14.95, 17.42, 13.37,
+];
+
+const lows = [
+  9.12, 19.57, 11.31, 17.94, 16.2, 8.81, 10.21, 12.95, 15.42, 11.46, 9.12,
+  20.04, 11.31, 17.94, 16.2, 8.73, 10.21, 12.95, 14.39, 11.46, 9.12, 19.57,
+  11.31, 17.94, 16.2, 13.26, 10.21, 12.95, 15.42, 11.37,
+];
+
+describe('aroon', () => {
+  it('should calculate the aroon based on the given parameters', () => {
+    const aroon = getAroon(highs, lows, 14);
+
+    expect(aroon).toStrictEqual({ aroonDown: 35.71, aroonUp: 42.86 });
+  });
+
+  it('should return null if the given period is greater than the high or low array', () => {
+    const aroon = getAroon(highs, lows, 32);
+
+    expect(aroon).toBe(null);
+  });
+});
